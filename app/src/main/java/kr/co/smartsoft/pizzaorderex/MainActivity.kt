c97f13a42dc5865672911170fa60014be428bd4d
이 성 노 <kulbo0926@gmail.com>
2022-02-27 21:00:15 +0900

    getCount /getItem 구현

--- app/src/main/java/kr/co/smartsoft/pizzaorderex/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/MainActivity.kt
@@ -2,10 +2,28 @@ package kr.co.smartsoft.pizzaorderex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.smartsoft.pizzaorderex.adapters.MainViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setupValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setupValues() {
+        mAdapter = MainViewPagerAdapter(supportFragmentManager)
+
+        mainViewPager.adapter = mAdapter
+
     }
 }
--- app/src/main/java/kr/co/smartsoft/pizzaorderex/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/adapters/MainViewPagerAdapter.kt
@@ -1,7 +1,21 @@
 package kr.co.smartsoft.pizzaorderex.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.smartsoft.pizzaorderex.fragments.MyProfileFragment
+import kr.co.smartsoft.pizzaorderex.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position) {
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
+    }
 }
