52166e0d65f10e4555c3f981de7b67dab71b0295
이 성 노 <kulbo0926@gmail.com>
2022-02-27 20:35:07 +0900

    뷰페이저 어댑터 클래스 제작중 - 상속 부분 해결

---
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,7 @@
+package kr.co.smartsoft.pizzaorderex.adapters
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+}
