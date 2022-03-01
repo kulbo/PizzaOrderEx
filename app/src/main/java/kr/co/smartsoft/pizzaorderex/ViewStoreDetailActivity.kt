d542c247e9222f6c736837c409f2e042bfe3d46f
이 성 노 <kulbo0926@gmail.com>
2022-03-01 21:09:55 +0900

    실제로 데이터 표시하기

--- app/src/main/java/kr/co/smartsoft/pizzaorderex/ViewStoreDetailActivity.kt
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/ViewStoreDetailActivity.kt
@@ -2,6 +2,11 @@ package kr.co.smartsoft.pizzaorderex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
+import kotlinx.android.synthetic.main.store_list_item.*
+import kotlinx.android.synthetic.main.store_list_item.imgLogo
+import kotlinx.android.synthetic.main.store_list_item.txtStoreName
 import kr.co.smartsoft.pizzaorderex.datas.StoreData
 
 class ViewStoreDetailActivity : AppCompatActivity() {
@@ -21,6 +26,12 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setValues() {
+        Glide.with(this).load(mStoreData.logoImageURL).into(imgLogo)
 
+        txtStoreName.text = mStoreData.name
+        txtPhoneNum.text = mStoreData.phoneNum
+
+        detailRatingBar.rating = mStoreData.rating.toFloat()
+        txtRating.text = "(${mStoreData.rating})"
     }
 }
