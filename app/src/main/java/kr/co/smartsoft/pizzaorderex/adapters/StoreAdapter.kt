9c067953abdb8d015b90fad1abbe38a5befc31ee
이 성 노 <kulbo0926@gmail.com>
2022-03-01 10:37:23 +0900

    getView 함수 기본 구조

--- app/src/main/java/kr/co/smartsoft/pizzaorderex/adapters/StoreAdapter.kt
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/adapters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package kr.co.smartsoft.pizzaorderex.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kr.co.smartsoft.pizzaorderex.R
 import kr.co.smartsoft.pizzaorderex.datas.StoreData
 
 class StoreAdapter(
@@ -9,4 +13,14 @@ class StoreAdapter(
     resId: Int,
     val mList:ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>(mContext, resId, mList) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 }
