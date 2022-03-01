b2073b786daa204816448c349d6713ff7beb5901
이 성 노 <kulbo0926@gmail.com>
2022-03-01 21:44:43 +0900

    권한승인시 > 실제 전화 연결 (CALL)

--- app/src/main/java/kr/co/smartsoft/pizzaorderex/ViewStoreDetailActivity.kt
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/ViewStoreDetailActivity.kt
@@ -1,13 +1,20 @@
 package kr.co.smartsoft.pizzaorderex
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 import kotlinx.android.synthetic.main.store_list_item.*
 import kotlinx.android.synthetic.main.store_list_item.imgLogo
 import kotlinx.android.synthetic.main.store_list_item.txtStoreName
 import kr.co.smartsoft.pizzaorderex.datas.StoreData
+import java.security.Permission
+import java.util.jar.Manifest
 
 class ViewStoreDetailActivity : AppCompatActivity() {
 
@@ -23,6 +30,27 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
     fun setupEvents() {
 
+//        권한 관련 변수를 화면이 만들어질때 한번만 세팅.
+        val pl = object : PermissionListener {
+            override fun onPermissionGranted() {
+                val myUri = Uri.parse("tel:${mStoreData.phoneNum}")
+                val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                startActivity(myIntent)
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@ViewStoreDetailActivity, "통화 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        btnCall.setOnClickListener {
+//          클릭될 때마다 만든 변수를 사용 만 하는 걸로.
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(android.Manifest.permission.CALL_PHONE)
+                .check()
+        }
     }
 
     fun setValues() {
