e05c3e6e23c993dbf451315aca87ec4a1dc8aaad
이 성 노 <kulbo0926@gmail.com>
2022-03-01 20:13:47 +0900

    리스트뷰의 이벤트 처리  - 단순 화면이동으로 먼저 구현

---
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/EditNicknameActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.smartsoft.pizzaorderex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+}
--- app/src/main/java/kr/co/smartsoft/pizzaorderex/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/fragments/PizzaStoreListFragment.kt
@@ -37,6 +37,7 @@ class PizzaStoreListFragment: Fragment() {
 
             val myIntent = Intent(requireContext(), ViewStoreDetailActivity::class.java)
             myIntent.putExtra("store", clickedStore)
+            startActivity(myIntent)
 
         }
 
