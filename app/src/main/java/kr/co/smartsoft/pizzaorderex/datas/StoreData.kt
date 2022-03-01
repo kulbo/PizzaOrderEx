31b358b446328d786084da03aaf7343626212f20
이 성 노 <kulbo0926@gmail.com>
2022-03-01 10:24:47 +0900

    가게 데이터 표현용 클래스 추가

---
+++ app/src/main/java/kr/co/smartsoft/pizzaorderex/datas/StoreData.kt
@@ -0,0 +1,10 @@
+package kr.co.smartsoft.pizzaorderex.datas
+
+class StoreData(
+    val name:String,
+    val rating:Double,
+    val phoneNum:String,    // 010 0이 없어짐
+    val logoImageURL:String,
+
+) {
+}
